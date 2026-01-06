8fdcfdfd0983012b1c3b3af9d5e08e5c7a7a91b5
NEERAJ-KR01 <neerajkumar9748087@gmail.com>
2026-01-06 21:32:19 +0530

    array_eg.sv

--- array_eg.sv
+++ array_eg.sv
@@ -4,5 +4,6 @@ module unpacked;
 initial begin
   cinema_seats= '{'{10,20},'{30,40},'{45,50}};
   $display("value %d %d %d %d",cinema_seats[0][0],cinema_seats[0][1],cinema_seats[2][0],cinema_seats[2][1]);
+  
   end
 endmodule
