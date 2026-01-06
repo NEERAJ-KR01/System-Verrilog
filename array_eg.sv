3c41edc6affde15d7a26c44bbb2eabb011a3f1ea
NEERAJ-KR01 <neerajkumar9748087@gmail.com>
2026-01-06 21:31:24 +0530

    Create array_eg.sv

---
+++ array_eg.sv
@@ -0,0 +1,8 @@
+
+module unpacked;
+  int cinema_seats[3][2];
+initial begin
+  cinema_seats= '{'{10,20},'{30,40},'{45,50}};
+  $display("value %d %d %d %d",cinema_seats[0][0],cinema_seats[0][1],cinema_seats[2][0],cinema_seats[2][1]);
+  end
+endmodule
