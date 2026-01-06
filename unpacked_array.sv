100b46f4da246f7b62d6ecad35b54bb84f559fd6
NEERAJ-KR01 <neerajkumar9748087@gmail.com>
2026-01-06 13:55:53 +0530

    unpacked_array.sv

--- unpacked_array.sv
+++ unpacked_array.sv
@@ -7,5 +7,13 @@ module unpacked;
     b[24]=1'b1;
     $display(a);
     $display(b);
+    $display("a=%b",a);
+    $display("b=%b",b);
+    a[3][7]=1;
+    $display("a=%b\t%d",a,a);
+    a[2]=8'b10000001; //row 2 8th nd 1 bit
+    $display("a=%b",a);
+    a[1]=8'h81; //row 1 8th nd 1 bit
+    $display("a=%b",a);
   end
 endmodule
