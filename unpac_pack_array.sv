65345827f7f881ab31c07432a1b77c76e18a18a6
NEERAJ-KR01 <neerajkumar9748087@gmail.com>
2026-01-06 22:51:58 +0530

    unpac-&-pack_array.sv

--- unpac_pack_array.sv
+++ unpac_pack_array.sv
@@ -1,5 +1,5 @@
 
-module array_eg1_;
+module unpac_pack_array_eg1_;
   bit[3:0][7:0]a; //32 bit data at 1 add
   bit[31:0]b; //32 bit data pkt at 1 add
   bit[7:0]c[3:0]; // 32 bit memory // 8 bit data at 4 loc
