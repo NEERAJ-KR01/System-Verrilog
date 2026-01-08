cd518c0ad11380d8588d7068c87d53508512d2d8
NEERAJ-KR01 <neerajkumar9748087@gmail.com>
2026-01-08 18:12:02 +0530

    datatype_$_bits.sv

--- datatype_$_bits.sv
+++ datatype_$_bits.sv
@@ -1,10 +1,12 @@
 module datatype_$_bits;
  bit [3:0][7:0]a; //8bt--4times
- bit[31:0]b; 
+ bit[31:0]b;
+ bit[7:0]c[3:0]; //8bt--4times
  initial begin 
  a[3]=8'b11111111;
  b[20]=1'b1;
- $display($bits(a[3]),$bits(b[20]));//ep: 8, 1
- $display($bits(a),$bits(b));//ep: 32 ,32
+ c[3][0]=1'b1;
+ $display($bits(a[3]),$bits(b[20]),$bits(c[0]));//ep: 8, 1,8
+ $display($bits(a),$bits(b),$bits(c));//ep: 32 ,32
  end
 endmodule
