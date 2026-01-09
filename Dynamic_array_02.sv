e9a50fc6a872f3b5a07d79745d03ca08751fa857
NEERAJ-KR01 <neerajkumar9748087@gmail.com>
2026-01-09 15:04:05 +0530

    Dynamic_array_02.sv

---
+++ Dynamic_array_02.sv
@@ -0,0 +1,11 @@
+`timescale 1ns / 1ps
+module Dynamic_array();
+byte da[]; //8 bit wide @ n loc dynamic array
+initial begin
+da=new[5]; //n=5
+foreach(da[i]) begin //da[i]=(i=0; i<=(n-1); i=i+1)
+ da[i]=$random;
+ $display("room:%d%d%b",i,da[i],da[i]);
+end
+end
+endmodule
