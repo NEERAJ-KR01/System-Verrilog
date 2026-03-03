30a7ced4d192973d8e1adcfbde0cb403ca2bb8fc
NEERAJ-KR01 <neerajkumar9748087@gmail.com>
2026-03-03 19:06:29 +0530

    mailbox.sv

--- mailbox.sv
+++ mailbox.sv
@@ -1,35 +1,41 @@
-class Tx;
-int i=25;
+class Tx;//Transmitter 
+int data =24;
 mailbox #(int) mbx1;
-function  new(mailbox #(int) mbx);
-mbx1=mbx; 
+  //constructor
+function new (mailbox #(int) mbx);
+  mbx1=mbx;
 endfunction
-task tx();
-mbx1.put(i);
-endtask
-endclass 
 
-class Rx;
-int i;
-mailbox #(int) mbx2;
-function new(mailbox #(int) mbx);
-mbx2=mbx;
-endfunction
-task rx();
-  mbx2.get(i);
+task tx ();
+  //.put() inbuilt in mailbox
+  mbx1.put(data);
+  $display("[%0t] Tx ka data :%0d mene pipeline me daal diya",$time,data);
 endtask
-endclass 
+endclass
 
+class Rx; //Reciever
+  int data;
+  mailbox #(int) mbx2;
+  function new(mailbox #(int) mbx);
+    mbx2=mbx;
+  endfunction 
+  task rx;
+    //.get() inbuilt in mailbox
+    mbx2.get(data); //inbuilt
+    $display("[%0t] Rx ne data :%0d pipeline se nikal liya hai",$time,data);
+  endtask 
+endclass
 module tb;
-Tx t_h; 
+Tx t_h;
 Rx r_h;
 mailbox #(int) mbx;
 initial begin
-mbx=new();
-t_h=new(mbx); r_h=new(mbx);
-$display("i from Tx=%0d",t_h.i);
-t_h.tx();
-r_h.rx();
-$display("i from Rx=%0d",r_h.i);
-end
-endmodule 
+  mbx=new();
+  t_h=new(mbx); r_h=new(mbx);
+  $display("im from Tx",t_h.data);
+  t_h.tx();
+  r_h.rx();
+  $display("im from Rx",r_h.data);
+end endmodule
+  
+  
